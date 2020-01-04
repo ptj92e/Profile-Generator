819b16582fe2f038b2afdfa76733a9c916d99671
Phillip Jones <ptj92e@gmail.com>
2020-01-04 08:21:09 -0600

    Finished Prompts

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 let inquirer = require("inquirer");
 let fs = require('fs');
 let electron = require('electron');
+let axios = require('axios');
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub user name?",
+    },
+    {
+        type: "list",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"],
+    }
+]).then(function(data) {
+
+})
