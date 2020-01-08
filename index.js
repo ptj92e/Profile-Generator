b6167a10cfe8b045d63b1ef042d95c1e330e0e3b
Phillip Jones <ptj92e@gmail.com>
2020-01-07 20:08:23 -0600

    Finished function to create an HTML page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let inquirer = require("inquirer");
 let fs = require('fs');
 let electron = require('electron');
 let axios = require('axios');
+let HTML = require('./generateHTML');
 // This function prompts the user with a series of questions
 inquirer.prompt([
     {
@@ -18,21 +19,48 @@ inquirer.prompt([
         message: "What is your favorite color?",
         choices: ["green", "blue", "pink", "red"],
     }
-//This information is passed to a function that 
-]).then(function(data, err) {
+    //This information is passed to a function that 
+]).then(function (data, err) {
     //This if statement is only here in case an error occurs during the questioning
     if (err) {
         return console.log(err);
     };
+    let bioPic = null;
+    let givenName = null;
+    let location = null;
+    let githubUsername = null;
+    let githubLink = null;
+    let blog = null;
+    let userBio = null;
+    let publicRepos = null;
+    let followers = null;
+    let stars = null;
+    let following = null;
+
     //This axios call is using the username collected from the prompts to call out to github
-    axios.get("https://api.github.com/users/" + data.username).then(function(res) {
+    axios.get("https://api.github.com/users/" + data.username).then(function (res) {
+        bioPic = res.data.avatar_url;
         givenName = res.data.name;
-        let userBio = res.data.bio;
-        let publicRepos = res.data.public_repos;
-        let followers = res.data.followers;
-        let following = res.data.following;
-        let blog = res.data.blog;
-        let githubUsername = res.data.login;
-        let githubLink = "https://github/" + githubUsername;
+        location = res.data.location;
+        githubUsername = res.data.login;
+        githubLink = "https://github.com/" + githubUsername;
+        blog = res.data.blog;
+        userBio = res.data.bio;
+        publicRepos = res.data.public_repos;
+        followers = res.data.followers;
+        following = res.data.following;
+
+        axios.get("https://api.github.com/users/" + data.username + "/starred").then(function (res) {
+            stars = res.data;
+            console.log(bioPic);
+            let htmlData = HTML.generateHTML(data, bioPic, givenName, location, githubLink, blog, userBio, publicRepos, followers, stars, following);
+
+            fs.writeFile("profile.html", htmlData, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("Success!");
+            });
+        });
     });
-})
+});
